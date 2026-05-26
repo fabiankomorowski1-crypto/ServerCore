01f4b0695c20a9da9adf71af805e6dcb94d61bbd
fabiankomorowski1-crypto <fabiankomorowski1@wp.pl>
2026-05-26 23:11:36 +0200

    Chat formatter utility - reserved for formatting implementation

---
+++ src/main/java/fabian/serverCore/formatting/ChatFormatter.java
@@ -0,0 +1,12 @@
+package fabian.serverCore.formatting;
+
+public class ChatFormatter {
+
+    // TODO: Space reserved for chat formatting implementation
+    // This class will handle:
+    // - Message appearance based on rank
+    // - Emote processing
+    // - Color application
+    // - Other formatting features
+
+}
